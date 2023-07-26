ffc1a6bc26b78bec1ff2f94570370a19897bfa0d
alexsylvian <lazeratus1@gmail.com>
2023-07-25 21:35:42 -0400

    first run, made a card

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+const toyButton = document.getElementById("create-toy")
+const toyList = document.getElementById("toy-list")
 let addToy = false;
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -13,3 +16,29 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function newToy() {
+  toyButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log("success?")
+  })
+}
+
+newToy()
+
+function renderToy(toy) {
+  let card = document.createElement('li')
+  card.className = ('card')
+  card.innerHTML = `
+  <img src = "${toy.image}"
+  `
+  toyList.appendChild(card)
+}
+
+function getAllToys(){
+  fetch(`http://localhost:3000/toys/2`)
+  .then(res => res.json())
+  .then(toys => renderToy(toys))
+}
+
+getAllToys()
